477c461b5efdad8a15a308a7bba938a97ce26586
Cody Lee <codylee02@gmail.com>
2019-08-15 14:42:40 -0600

    Componetized Summary of selected items

--- src/Composition/Summary/Summary.js
+++ src/Composition/Summary/Summary.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import Total from '../Total/Total'
 
 class Summary extends React.Component {
     render() {
+
+        const summary = Object.keys(this.props.selected).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const selectedOption = this.props.selected[feature];
+      
+            return (
+              <div className="summary__option" key={featureHash}>
+                <div className="summary__option__label">{feature} </div>
+                <div className="summary__option__value">{selectedOption.name}</div>
+                <div className="summary__option__cost">
+                  {this.props.USCurrencyFormat.format(selectedOption.cost)}
+                </div>
+              </div>
+            );
+          });
+
+
         return (
-            <section className="main__summary">
-                <h2>Your cart</h2>
-                {/* {SUMMARY GGOES HERE} */}
-                <Total />
-            </section>
+            <>{summary}</>
             
         )
     }
